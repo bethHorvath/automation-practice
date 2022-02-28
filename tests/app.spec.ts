7fc37ec9dae953892505423be6e65bef6d7f4750
Bethany <bethany.horvath@sky.uk>
2022-02-28 11:56:42 +0000

    deleted learn react test

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,15 +1,11 @@
-import { test, expect, Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('localhost:3000');
 });
 
 test.describe('testing react app', () => {
-  test('should render Learn React text', async ({page}) => {
-      expect(page.locator('text=Learn React')).toBeDefined();
-  });
-
     test('should render Lets Start Automating text', async ({page}) => {
-      expect(page.locator('text=Lets Start Automating')).toBeDefined()
+      expect(page.locator('text=Lets Start Automating')).toBeTruthy()
     });
 });

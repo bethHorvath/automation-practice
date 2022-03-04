2930a530287cf89b3f94154a3662569730aed3b9
Bethany <bethany.horvath@sky.uk>
2022-03-04 14:21:42 +0000

    Deleted autogenerated files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p className="text">Lets start automating!</p>
         <a className="App-link" href="https://www.sky.com/" target="_blank" rel="noopener noreferrer">
           Sky website

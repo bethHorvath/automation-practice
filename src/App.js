d4066b2864065fb8d7025f9839cbecaaf2dd41cc
Bethany <bethany.horvath@sky.uk>
2022-04-11 13:50:35 +0100

    Changed app to use styled components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+// import './App.css';
 
-import './App.css';
+const StyledApp = styled.div`
+  text-align: center;
+`;
+
+const StyledParagraph = styled.p`
+  color: red;
+`;
+
+const StyledHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const StyledLink = styled.a`
+  color: #61dafb;
+`;
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="text">Lets start automating!</p>
-        <a className="App-link" href="https://www.sky.com/" target="_blank" rel="noopener noreferrer">
+    <StyledApp>
+      <StyledHeader>
+        <StyledParagraph>Lets start automating!</StyledParagraph>
+        <StyledParagraph>But first, we are learning about the DOM!</StyledParagraph>
+        <StyledParagraph>Practicing CSS!</StyledParagraph>
+        <StyledLink href="https://www.sky.com/" target="_blank" rel="noopener noreferrer">
           Sky website
-        </a>
-      </header>
-    </div>
+        </StyledLink>
+      </StyledHeader>
+    </StyledApp>
   );
 };
 

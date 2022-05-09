b9abb605a7bf442a6b226ed8bdf5f982cdec6fe6
CraigBallard <craig.ballard@sky.uk>
2022-05-09 17:03:33 +0100

    Test for confirming calendar href

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -18,9 +18,17 @@ test.describe('testing react app', () => {
     await expect(page.locator('text=Sky website')).toBeVisible();
   });
 
-  test.only('text should display when a key is pressed', async ({ page }) => {
+  test('text should display when a key is pressed', async ({ page }) => {
     await page.focus('data-test-id=key-press-input');
     await page.keyboard.press('Enter');
     await expect(page.locator('text=you pressed Enter')).toBeVisible();
+  });
+
+  test('Team Calendar is displayed as link on app page', async ({ page }) => {
+    const calendarText = await page.locator('text=Team Calendar');
+    await expect(calendarText).toHaveAttribute(
+      'href',
+      'https://wiki.at.sky/display/LT/Digital+Service+Test+-+Team+Calendar'
+    );
   });
 });

eb719b56ee9291230056a70497ee8ea796ce2322
CraigBallard <craig.ballard@sky.uk>
2022-05-09 16:44:01 +0100

    pushing up my changes

--- src/craig-app.js
+++ src/craig-app.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+import { confluenceLinks } from './constants';
+
 // import './App.css';
 
 const StyledApp = styled.div`
@@ -32,10 +34,10 @@ const random = number => {
 
 const changeStyling = () => {
   console.log('The button has been clicked and the width, font family and background color has been changed');
-  document.body.style.width = '30%'
+  document.body.style.width = '30%';
   const rdmCol = `rgb(${random(255)},${random(255)},${random(255)})`;
   document.body.style.backgroundColor = rdmCol;
-  document.body.style.fontFamily = 'monospace'; 
+  document.body.style.fontFamily = 'monospace';
 };
 
 const handleSubmit = e => {
@@ -46,47 +48,54 @@ const handleSubmit = e => {
 
 const App = () => {
   const [keyPress, setKeyPress] = useState('');
-  
+
   const keyPressFunction = e => {
-  switch (e.key) {
-  case 'd':
-  console.log('You have hit d');
-  break;
-  case 'Enter':
-  console.log('You have hit Enter');
-  break;
-  case 't':
-  console.log('You have hit t');
-  break
-  default: 
-  console.log('You have hit something you should not have') 
-};
-  }
+    switch (e.key) {
+      case 'd':
+        console.log('You have hit d');
+        break;
+      case 'Enter':
+        console.log('You have hit Enter');
+        break;
+      case 't':
+        console.log('You have hit t');
+        break;
+      default:
+        console.log('You have hit something you should not have');
+    }
+  };
 
   const outputPrinter = e => {
-      keyPressFunction(e);
-      setKeyPress(`you pressed ${e.key}`);
+    keyPressFunction(e);
+    setKeyPress(`you pressed ${e.key}`);
   };
+
+  const confluenceDocs = () =>
+    confluenceLinks.map((confluenceEl, i) => (
+      <StyledLink key={i} href={confluenceEl.link}>
+        {confluenceEl.name}
+      </StyledLink>
+    ));
 
   return (
     <StyledApp>
       <StyledHeader>
         <StyledParagraph>Lets start automating!</StyledParagraph>
-        <StyledParagraph>But first, we are learning about the DOM'</StyledParagraph>
+        <StyledParagraph>But first, we are learning about the DOM</StyledParagraph>
         <StyledParagraph>practising CSS</StyledParagraph>
         <StyledLink href="https://www.sky.com/" target="_blank" rel="noopener noreferrer">
           Sky website
         </StyledLink>
+        {confluenceDocs()}
         <button type="button" onDoubleClick={changeStyling}>
-            changeStyling
-            </button>
-            <form onSubmit={handleSubmit}>
-                <button type="submit">Submit</button>
-            </form>
-            <input id="textbox" type= "text" onKeyDown={outputPrinter} />
+          changeStyling
+        </button>
+        <form onSubmit={handleSubmit}>
+          <button type="submit">Submit</button>
+        </form>
+        <input id="textbox" type="text" onKeyDown={outputPrinter} data-test-id="key-press-input" />
 
-
-            <div id="output">{keyPress}</div>
+        <div id="output">{keyPress}</div>
       </StyledHeader>
     </StyledApp>
   );

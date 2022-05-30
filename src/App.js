44764e762788a3c1193b9d8b69ca757001dbf5e6
Ben Mackay <ben.mackay@sky.uk>
2022-05-30 15:48:30 +0100

    Co-authored-by: Angus Wilson <Angus83@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -4,182 +4,94 @@ import styled from 'styled-components';
 import { confluenceLinks } from './constants';
 
 const StyledApp = styled.div`
-  text-align: left;
+  text-align: center;
+`;
+
+const StyledParagraph = styled.p`
+  color: red;
 `;
 
 const StyledHeader = styled.header`
-  color: black;
-  background-color: #fff;
-  min-height: 10vh;
-  margin-left: 20px;
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
 `;
 
-const StyledTitle = styled.p`
-  font-size: 28px;
+const StyledLink = styled.a`
+  color: #61dafb;
 `;
 
-const StyledSubTitle = styled.p`
-  font-size: 12px;
-  color: #5e6c84;
-`;
+const random = number => {
+  return Math.floor(Math.random() * (number + 1));
+};
 
-const StyledGrid = styled.div`
-  max-width: 896px;
-  border: 1px solid #c1c7d0;
-`;
+const backgroundChange = () => {
+  return `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+};
 
-const StyledTitleRow = styled.div`
-  display: flex;
-  color: rgb(0, 0, 255);
-  background-color: #e6fcff;
-  text-align: center;
-  height: 35px;
-  font-weight: bold;
-`;
+const changeStyling = () => {
+  console.log('The button has been clicked and the width and font family has been changed');
+  document.body.style.width = '30%';
+  document.body.style.backgroundColor = backgroundChange();
+  document.body.style.fontFamily = 'monospace';
+};
 
-const StyledPictureRow = styled.div`
-  display: flex;
-  text-align: center;
-  height: 146px;
-`;
-
-const StyledCol = styled.div`
-  flex: ${props => props.size};
-  border: 1px solid #c1c7d0;
-  padding: 7px;
-`;
-
-const StyledImage = styled.img`
-  width: 100px;
-  height: 100px;
-  margin: 23px;
-`;
-
-const StyledLink = styled.a``;
+const keyPressFunction = e => {
+  switch (e.key) {
+    case 'Enter':
+      console.log('you hit enter');
+      break;
+    case 'd':
+      console.log('you hit d');
+      break;
+    default:
+      console.log('you hit something else');
+  }
+};
 
 const App = () => {
+  const [keyPress, setKeyPress] = useState('');
+
+  const outputPrinter = e => {
+    keyPressFunction(e);
+    setKeyPress(`you pressed: ${e.key}`);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(`the event is: ${e.type} and this was triggered on:`, e.target);
+  };
+
+  const confluenceDocs = () =>
+    confluenceLinks.map((confluenceEl, i) => (
+      <StyledLink key={i} href={confluenceEl.link}>
+        {confluenceEl.name}
+      </StyledLink>
+    ));
+
   return (
     <StyledApp data-test-id="body">
       <StyledHeader>
-        <StyledTitle>Test Chapter</StyledTitle>
-        <StyledSubTitle>
-          Created by Field, James (Agile Test Lead), last modified by Avery, Luke (Scrum Master) on May 19, 2022
-        </StyledSubTitle>
-        <StyledGrid>
-          <StyledTitleRow>
-            <StyledCol size={1}>Team Calendar</StyledCol>
-            <StyledCol size={1}>Team Process</StyledCol>
-            <StyledCol size={1}>Test Data Hub</StyledCol>
-            <StyledCol size={1}>Team Strategy</StyledCol>
-          </StyledTitleRow>
-          <StyledPictureRow>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/Digital+Service+Test+-+Team+Calendar">
-                <StyledImage src="https://cdn-icons-png.flaticon.com/128/3176/3176395.png" />
-              </StyledLink>
-            </StyledCol>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/Digital+Service+-+Team+Process">
-                <StyledImage src="https://cdn-icons-png.flaticon.com/128/4149/4149680.png" />
-              </StyledLink>
-            </StyledCol>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/01+Test+Data">
-                <StyledImage src="https://cdn-icons-png.flaticon.com/128/2920/2920349.png" />
-              </StyledLink>
-            </StyledCol>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/Digital+Service+-+Test+-+Team+Strategy">
-                <StyledImage src="https://cdn-icons-png.flaticon.com/128/609/609085.png" />
-              </StyledLink>
-            </StyledCol>
-          </StyledPictureRow>
-          <StyledTitleRow>
-            <StyledCol size={1}>Team Handover Guides</StyledCol>
-            <StyledCol size={1}>Team Structure</StyledCol>
-            <StyledCol size={1}>Archive</StyledCol>
-            <StyledCol size={1}>SQL Queries</StyledCol>
-          </StyledTitleRow>
-          <StyledPictureRow>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/Digital+Services+Test+-+Squad+guides">
-                <StyledImage src="https://cdn-icons-png.flaticon.com/128/1705/1705312.png" />
-              </StyledLink>
-            </StyledCol>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/Service+Test+Chapter+Structure">
-                <StyledImage src="https://cdn-icons-png.flaticon.com/128/848/848525.png" />
-              </StyledLink>
-            </StyledCol>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/Digital+Service+Test+-+Archive">
-                <StyledImage src="https://cdn-icons-png.flaticon.com/128/432/432607.png" />
-              </StyledLink>
-            </StyledCol>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/The+SQL+Page">
-                <StyledImage src="https://cdn-icons-png.flaticon.com/128/337/337953.png" />
-              </StyledLink>
-            </StyledCol>
-          </StyledPictureRow>
-          <StyledTitleRow>
-            <StyledCol size={1}>Automation</StyledCol>
-            <StyledCol size={1}>Tools</StyledCol>
-            <StyledCol size={1}>NDA</StyledCol>
-            <StyledCol size={1}>Demo Schedule</StyledCol>
-          </StyledTitleRow>
-          <StyledPictureRow>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/02+Automation">
-                <StyledImage src="https://cdn-icons-png.flaticon.com/128/2289/2289266.png" />
-              </StyledLink>
-            </StyledCol>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/03+Tools">
-                <StyledImage src="https://cdn-icons-png.flaticon.com/128/1055/1055683.png" />
-              </StyledLink>
-            </StyledCol>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/Digital+Service+Test+-+NDA">
-                <StyledImage src="https://cdn-icons-png.flaticon.com/128/2716/2716654.png" />
-              </StyledLink>
-            </StyledCol>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/Service+Friday+Demo+Schedule">
-                <StyledImage src="https://t4.ftcdn.net/jpg/04/27/33/25/240_F_427332571_IeoccXzXqutsPw929RThSJUKfMoXlCjt.jpg" />
-              </StyledLink>
-            </StyledCol>
-          </StyledPictureRow>
-          <StyledTitleRow>
-            <StyledCol size={1}>My Sky App</StyledCol>
-            <StyledCol size={1}>Accessibility</StyledCol>
-            <StyledCol size={1}>Test Library</StyledCol>
-            <StyledCol size={1}>Training Videos</StyledCol>
-          </StyledTitleRow>
-          <StyledPictureRow>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/My+Sky+App+Testing">
-                <StyledImage src="https://play-lh.googleusercontent.com/qny_nvVQwuziiMRuq9Vs5cnpKrXfP84g7xyaW21b_fFLO1JhGkFSx1Kr9qMKg_vbBZTu" />
-              </StyledLink>
-            </StyledCol>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/Digital+Service+-+Accessibility+Test+Strategy">
-                <StyledImage src="https://cdn-icons-png.flaticon.com/128/2463/2463039.png" />
-              </StyledLink>
-            </StyledCol>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/Test+Library">
-                <StyledImage src="https://www.ghil.ac.uk/fileadmin/_processed_/1/6/csm_German-Historical-Institute-London_HR0024_e6c6419f96.jpg" />
-              </StyledLink>
-            </StyledCol>
-            <StyledCol size={1}>
-              <StyledLink href="https://wiki.at.sky/display/LT/Trainning+Video">
-                <StyledImage src="https://cdn-icons-png.flaticon.com/128/906/906175.png" />
-              </StyledLink>
-            </StyledCol>
-          </StyledPictureRow>
-        </StyledGrid>
+        <StyledParagraph className="text">Lets start automating!</StyledParagraph>
+        <StyledParagraph className="text">But first, we are learning about the DOM!</StyledParagraph>
+        <StyledParagraph className="text">Practicing CSS</StyledParagraph>
+        <StyledLink href="https://www.sky.com/" target="_blank" rel="noopener noreferrer">
+          Sky website
+        </StyledLink>
+        {confluenceDocs()}
+        <button type="button" onClick={changeStyling} data-test-id="change-colour-button">
+          Change color
+        </button>
+        <form onSubmit={handleSubmit}>
+          <button type="submit">Submit</button>
+        </form>
+        <input id="textbox" type="text" onKeyDown={outputPrinter} data-test-id="key-press-input" />
+        <div id="output">{keyPress}</div>
       </StyledHeader>
     </StyledApp>
   );
---
+++ src/team-tasks/testConfluenceTasksConstants.js
@@ -0,0 +1,84 @@
+export const confluenceLinks = [
+  {
+    name: 'Team Calendar',
+    link: 'https://wiki.at.sky/display/LT/Digital+Service+Test+-+Team+Calendar',
+    imgLink: 'https://cdn-icons-png.flaticon.com/128/3176/3176395.png',
+  },
+  {
+    name: 'Team Process',
+    link: 'https://wiki.at.sky/display/LT/Digital+Service+-+Team+Process',
+    imgLink: 'https://cdn-icons-png.flaticon.com/128/4149/4149680.png',
+  },
+  {
+    name: 'Test Data Hub',
+    link: 'https://wiki.at.sky/display/LT/01+Test+Data',
+    imgLink: 'https://cdn-icons-png.flaticon.com/128/2920/2920349.png',
+  },
+  {
+    name: 'Team Strategy',
+    link: 'https://wiki.at.sky/display/LT/Digital+Service+-+Test+-+Team+Strategy',
+    imgLink: 'https://cdn-icons-png.flaticon.com/128/609/609085.png',
+  },
+  {
+    name: 'Team Handover Guides',
+    link: 'https://wiki.at.sky/display/LT/Digital+Services+Test+-+Squad+guides',
+    imgLink: 'https://cdn-icons-png.flaticon.com/128/1705/1705312.png',
+  },
+  {
+    name: 'Team Structure',
+    link: 'https://wiki.at.sky/display/LT/Service+Test+Chapter+Structure',
+    imgLink: 'https://cdn-icons-png.flaticon.com/128/848/848525.png',
+  },
+  {
+    name: 'Archive',
+    link: 'https://wiki.at.sky/display/LT/Digital+Service+Test+-+Archive',
+    imgLink: 'https://cdn-icons-png.flaticon.com/128/2289/2289266.png',
+  },
+  {
+    name: 'SQL Queries',
+    link: 'https://wiki.at.sky/display/LT/The+SQL+Page',
+    imgLink: 'https://cdn-icons-png.flaticon.com/128/337/337953.png',
+  },
+  {
+    name: 'Automation',
+    link: 'https://wiki.at.sky/display/LT/02+Automation',
+    imgLink: 'https://cdn-icons-png.flaticon.com/128/609/609085.png',
+  },
+  {
+    name: 'Tools',
+    link: 'https://wiki.at.sky/display/LT/03+Tools',
+    imgLink: 'https://cdn-icons-png.flaticon.com/128/1055/1055683.png',
+  },
+  {
+    name: 'NDA',
+    link: 'https://wiki.at.sky/display/LT/Digital+Service+Test+-+NDA',
+    imgLink: 'https://cdn-icons-png.flaticon.com/128/2716/2716654.png',
+  },
+  {
+    name: 'Demo Schedule',
+    link: 'https://wiki.at.sky/display/LT/Service+Friday+Demo+Schedule',
+    imgLink: 'https://t4.ftcdn.net/jpg/04/27/33/25/240_F_427332571_IeoccXzXqutsPw929RThSJUKfMoXlCjt.jpg',
+  },
+  {
+    name: 'My Sky App',
+    link: 'https://wiki.at.sky/display/LT/My+Sky+App+Testing',
+    imgLink:
+      'https://play-lh.googleusercontent.com/qny_nvVQwuziiMRuq9Vs5cnpKrXfP84g7xyaW21b_fFLO1JhGkFSx1Kr9qMKg_vbBZTu',
+  },
+  {
+    name: 'Accessibility',
+    link: 'https://wiki.at.sky/display/LT/Digital+Service+-+Accessibility+Test+Strategy',
+    imgLink: 'https://cdn-icons-png.flaticon.com/128/2463/2463039.png',
+  },
+  {
+    name: 'Test Library',
+    link: 'https://wiki.at.sky/display/LT/Test+Library',
+    imgLink:
+      'https://www.ghil.ac.uk/fileadmin/_processed_/1/6/csm_German-Historical-Institute-London_HR0024_e6c6419f96.jpg',
+  },
+  {
+    name: 'Training Videos',
+    link: 'https://wiki.at.sky/display/LT/Digital+Service+-+Test+-+Team+Strategy',
+    imgLink: 'https://cdn-icons-png.flaticon.com/128/906/906175.png',
+  },
+];
